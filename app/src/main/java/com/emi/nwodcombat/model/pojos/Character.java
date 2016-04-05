615ab77a59cf8ee48327a8f26e7e01baa22d3be6
Emiliano De Santis <emiliano.desantis@globant.com>
2016-04-05 18:09:00 -0300

    Tried to update Realm to latest version, came upon duplicate entry ZipException. (Joy.)

---
+++ app/src/main/java/com/emi/nwodcombat/model/pojos/Character.java
@@ -0,0 +1,12 @@
+package com.emi.nwodcombat.model.pojos;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by emiliano.desantis on 05/04/2016.
+ */
+public class Character extends RealmObject {
+    private String name;
+    private String player;
+    private String concept;
+}
